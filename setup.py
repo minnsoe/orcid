803337e1c48d551898790c5da5d2eed3f3d89a63
Minn Soe <contributions@minn.so>
2014-07-31 20:08:18 +0200

    convention: convert tabs to spaces

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 
 _parameters = {
-	'name': 'orcid',
-	'version': '0.1.1',
-	'description': 'ORCID API Client.',
-	'author': 'Minn Soe',
-	'maintainer': 'Minn Soe',
-	'maintainer_email': 'contributions@minn.so',
-	'license': 'BSD',
-	'packages': ['orcid'],
-	'classifiers': [
-		'License :: OSI Approved :: BSD License',
-		'Programming Language :: Python :: 2.7'
-	],
-	'install_requires': ['rauth']
+    'name': 'orcid',
+    'version': '0.1.1',
+    'description': 'ORCID API Client.',
+    'author': 'Minn Soe',
+    'maintainer': 'Minn Soe',
+    'maintainer_email': 'contributions@minn.so',
+    'license': 'BSD',
+    'packages': ['orcid'],
+    'classifiers': [
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    'install_requires': ['rauth']
 }
 
 
